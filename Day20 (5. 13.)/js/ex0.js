f6cc7bbf6abdf4eded8d191cae4c6e56a1db2a04
unao6 <102839531+unao6@users.noreply.github.com>
2022-06-23 18:34:04 +0900

    Add files via upload

---
+++ Day20 (5. 13.)/js/ex0.js
@@ -0,0 +1,6 @@
+const startBtn= document.querySelector('.start');
+const stopBtn= document.querySelector('.stop');
+const view= document.querySelector('.view');
+const output= document.querySelector('.output');
+
+let timer= null, count=0, interval= 1000;
